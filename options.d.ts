6c190bd1c1516ec694c22655886b3d5610639913
高伟健 <921593174@qq.com>
2024-07-26 11:37:17 +0800

    fix: optional type

--- options.d.ts
+++ options.d.ts
@@ -1,7 +1,7 @@
 export interface IPluginOptions {
-  scope: string;
-  sw_name: string;
-  cache_name: string;
-  static_files: string[];
-  patten: string;
+  scope?: string;
+  sw_name?: string;
+  cache_name?: string;
+  static_files?: string[];
+  patten?: string;
 }
